7d50dbb55df4f15bb09d2c1a81999925f71c30df
williamc373 <wicrowell@s.sfusd.edu>
2025-12-12 11:21:59 -0800

    M9L4 complete!

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -4,7 +4,18 @@ const UP_LEFT = Vector2.UP + Vector2.LEFT
 const UP_RIGHT = Vector2.UP + Vector2.RIGHT
 const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+
+const RUNNER = preload("uid://c4jjlttvtvjqc")
+const RUNNER_DOWN = preload("uid://c0i1ik45p7rhh")
+const RUNNER_DOWN_RIGHT = preload("uid://cst3aklarj68")
+const RUNNER_RIGHT = preload("uid://b4etxv4c5w1mq")
+const RUNNER_UP = preload("uid://dtrvq16cx035")
+const RUNNER_UP_RIGHT = preload("uid://c7x3s5c2r5l86")
+
 var max_speed := 600.0
+
+@onready var _skin: Sprite2D = %_skin
+
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -25,15 +36,3 @@ func _physics_process(_delta: float) -> void:
 			_skin.texture = RUNNER_DOWN_RIGHT
 	if direction_discrete.length() > 0:
 		_skin.flip_h = direction.x < 0.0
-
-
-
-
-@onready var _skin: Sprite2D = %_skin
-
-const RUNNER = preload("uid://c4jjlttvtvjqc")
-const RUNNER_DOWN = preload("uid://c0i1ik45p7rhh")
-const RUNNER_DOWN_RIGHT = preload("uid://cst3aklarj68")
-const RUNNER_RIGHT = preload("uid://b4etxv4c5w1mq")
-const RUNNER_UP = preload("uid://dtrvq16cx035")
-const RUNNER_UP_RIGHT = preload("uid://c7x3s5c2r5l86")
