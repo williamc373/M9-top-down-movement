111d1cd1f955a2bdbf6be3235c643eca9d3b8c8a
williamc373 <wicrowell@s.sfusd.edu>
2025-12-17 14:04:01 -0800

    M9L6 complete
    I finshed this lesson a couple days ago but i accidentally deleted my scene and had to figure that out.

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,33 +1,27 @@
 extends CharacterBody2D
-
-
-
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
-@export var decceleration :=1080.0
-
-@onready var _runner_visual: RunnerVisual = %RunnerVisualRed
-
-
-
+@export var deceleration := 1080.0
+@onready var _runner_visual_red: RunnerVisual = %RunnerVisualRed
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
 	var has_input_direction := direction.length() > 0.0
 	if has_input_direction:
 		var desired_velocity := direction * max_speed
 		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, decceleration * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	move_and_slide()
+	
 	if direction.length() > 0.0:
-		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
-		_runner_visual.animation_name = RunnerVisual.Animations.WALK
+		_runner_visual_red.angle = rotate_toward(_runner_visual_red.angle, direction.orthogonal().angle(),8.0 * delta)
 		var current_speed_percent := velocity.length() / max_speed
-		_runner_visual.animation_name = (
+		_runner_visual_red.animation_name = (
 			RunnerVisual.Animations.WALK
 			if current_speed_percent < 0.8
-			else RunnerVisual.Animations.RUN
+			else RunnerVisual.Animations.RUN	
 		)
 	else:
-		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_runner_visual_red.animation_name = RunnerVisual.Animations.IDLE
+	
