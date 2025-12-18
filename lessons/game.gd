6678617f08dfd30d2f857e6b2dab2b328299d067
williamc373 <wicrowell@s.sfusd.edu>
2025-12-18 09:14:53 -0800

    M9L9 complete

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var _finish_line: FinishLine = %FinishLine
-
+@onready var _count_down: CountDown = %CountDown
+@onready var _runner: Runner = %Runner
 func _ready() -> void:
 	_finish_line.body_entered.connect(func (body: Node) -> void:
 		if body is not Runner:
@@ -23,3 +24,10 @@ func _ready() -> void:
 	_finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
 	)
+	_count_down.start_counting()
+	_runner.set_physics_process(false)
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_runner.set_physics_process(true)
+	)
+	
